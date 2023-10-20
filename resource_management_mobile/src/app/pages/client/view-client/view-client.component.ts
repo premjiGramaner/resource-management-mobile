0f69629f60d5426a4d0561b8d53d07527db013a1
Devi Prasad <deviprasad.kittu@gmail.com>
2023-10-20 11:24:16 +0530

    Merge conflict code

--- resource_management_mobile/src/app/pages/client/view-client/view-client.component.ts
+++ resource_management_mobile/src/app/pages/client/view-client/view-client.component.ts
@@ -1,26 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-<<<<<<< HEAD
-=======
 import { DuplicateRemoverPipe } from 'src/app/shared/helpers/pipes/duplicate-remover.pipe';
 import { ClientArrayData } from '../models/client.model';
 import { ClientService } from '../service/client.service';
->>>>>>> f9ee66405f19c5d35faae098942adb9796dece44
 @Component({
   selector: 'app-view-client',
   templateUrl: './view-client.component.html',
   styleUrls: ['./view-client.component.scss'],
   standalone: true,
-<<<<<<< HEAD
-  imports: [IonicModule, CommonModule],
-})
-export class ViewClientComponent implements OnInit {
-  @Input() viewClientData: any;
-  constructor() { }
-
-  ngOnInit() { }
-=======
   imports: [IonicModule, CommonModule, DuplicateRemoverPipe],
 })
 export class ViewClientComponent implements OnInit {
@@ -42,5 +30,4 @@ export class ViewClientComponent implements OnInit {
       })
     }
   }
->>>>>>> f9ee66405f19c5d35faae098942adb9796dece44
 }
