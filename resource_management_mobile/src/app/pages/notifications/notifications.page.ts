ddba5e4c8abc62d7457432776a8a8900d3f54204
nallaiya <dineshbluesky1@gmail.com>
2023-11-08 19:20:39 +0530

    Notification UI designed

--- resource_management_mobile/src/app/pages/notifications/notifications.page.ts
+++ resource_management_mobile/src/app/pages/notifications/notifications.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from './services/notification.service';
+import { HiringInfo, notificationResponce } from './model/notification.model';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-notifications',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit {
+  notificationData: HiringInfo[] = [];
+  skip: number = 0;
+  searchQuery: string = '';
+  showSearch: boolean = false;
+  constructor(private notificationService: NotificationService) {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.getNotification(this.skip, 20, this.searchQuery);
+  }
+
+  getNotification(skip: number, limit: number, search: string) {
+    this.notificationService
+      .getNotification(skip, limit, search)
+      .subscribe((data: notificationResponce) => {
+        this.notificationData = [...this.notificationData, ...data.data.HiringInfo];
+        console.log(this.notificationData);
+
+      });
+  }
+
+  onIonInfinite(ev: any) {
+    this.skip = this.skip + 20;
+    this.getNotification(this.skip, 20, this.searchQuery);
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
 }
