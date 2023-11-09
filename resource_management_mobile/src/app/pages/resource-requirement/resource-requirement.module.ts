985f6dc6d28f29a7c5b29d6f2ac737c3522030b9
nallaiya <dineshbluesky1@gmail.com>
2023-11-09 18:20:06 +0530

    code formatted

--- resource_management_mobile/src/app/pages/resource-requirement/resource-requirement.module.ts
+++ resource_management_mobile/src/app/pages/resource-requirement/resource-requirement.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { ResourceRequirementPageRoutingModule } from './resource-requirement-routing.module';
-
 import { ResourceRequirementPage } from './resource-requirement.page';
 import { AddResourceRequirementComponent } from './add-resource-requirement/add-resource-requirement.component';
 import { HttpClientModule } from '@angular/common/http';
