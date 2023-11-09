ffa864d5501a11bd9c44e200ec0275e4c01b7b72
nallaiya <dineshbluesky1@gmail.com>
2023-11-09 17:25:47 +0530

    Implemented resource-requirement

---
+++ resource_management_mobile/src/app/pages/resource-requirement/add-resource-requirement/add-resource-requirement.component.ts
@@ -0,0 +1,147 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastConstants } from 'src/app/core/constant/toast.message.constant';
+import { ToastService } from 'src/app/core/toast/toast.service';
+import { RequirementService } from '../../requirement/services/requirement.service';
+import { ProfileService } from '../../profile/service/profile.service';
+import { IonItemSliding } from '@ionic/angular';
+import { Status } from 'src/app/core/enum/status.enum';
+import { Modules } from 'src/app/core/enum/static.enum';
+import {
+  resourceData,
+  resourceEntireData,
+  viewResourceData,
+} from '../models/resource-requirement-model';
+import { userProfile } from 'src/app/core/base-model/base.model';
+
+@Component({
+  selector: 'app-add-resource-requirement',
+  templateUrl: './add-resource-requirement.component.html',
+  styleUrls: ['./add-resource-requirement.component.scss'],
+})
+export class AddResourceRequirementComponent implements OnInit, OnChanges {
+  resourceForm!: FormGroup;
+  isModalOpen = false;
+  isEnableEdit: boolean = false;
+  module: string = Modules.Resource_requirement;
+  onSubmit: boolean = true;
+  userData: userProfile[] = [];
+  requirementData: resourceEntireData[] = [];
+  resourceId!: number;
+  @Input() flag!: string;
+  @Input() ViewRespurceData!: viewResourceData;
+  addedResource: resourceData[] = [];
+  constructor(
+    private toastConstants: ToastConstants,
+    private toastService: ToastService,
+    private requirementService: RequirementService,
+    private profileService: ProfileService
+  ) { }
+
+  ngOnInit() {
+    this.getRequirementService();
+    this.userService();
+
+    this.resourceForm = new FormGroup({
+      Requirement_requirement_id: new FormControl('', Validators.required),
+      evaluated_by: new FormControl('', Validators.required),
+      resources: new FormControl([]),
+      evaluated_date: new FormControl('', Validators.required),
+      comments: new FormControl('', Validators.required),
+      requirement: new FormControl(''),
+      evaluated_by_name: new FormControl(''),
+    });
+
+    if (this.ViewRespurceData != undefined) {
+      this.resourceForm.patchValue({
+        comments: this.ViewRespurceData.comments,
+        Requirement_requirement_id:
+          '' + this.ViewRespurceData.Requirement_requirement_id,
+        evaluated_by: '' + this.ViewRespurceData.evaluated_by,
+        evaluated_date: this.ViewRespurceData.evaluated_date.replace(
+          /\//g,
+          '-'
+        ),
+        evaluated_by_name: this.ViewRespurceData.evaluated_by_name,
+        requirement: this.ViewRespurceData.requirement,
+      });
+      this.resourceId = this.ViewRespurceData.Requirement_requirement_id;
+      this.addedResource =
+        this.ViewRespurceData.ResourceRequirementMappings.map((item: any) => {
+          const { resourceName, stageDescription, statusDescription, ...rest } =
+            item;
+          return {
+            resource_name: resourceName,
+            stage_description: stageDescription,
+            status_description: statusDescription,
+            ...rest,
+          };
+        });
+      this.addedResource.map((item: resourceData) => {
+        this.resourceForm.value.resources.push({
+          resource_id: item.Resource_resource_id,
+          stage_id: item.Stage_stage_id,
+          status_id: item.Status_status_id,
+        });
+      });
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.flag.toLowerCase() == Status.SAVE.toLowerCase()) {
+      this.isEnableEdit = false;
+    } else {
+      this.isEnableEdit = true;
+    }
+  }
+
+  getRequirementService() {
+    this.requirementService.getRequirementAllData().subscribe((requirement) => {
+      this.requirementData = requirement.data.requirementInfo;
+    });
+  }
+
+  requirementSelected(event: number) {
+    this.resourceId = event;
+  }
+
+  userService() {
+    this.profileService.getUser().subscribe((user) => {
+      this.userData = user.data.userInfo;
+    });
+  }
+
+  getSelectedResource(event: resourceData) {
+    const ids = {
+      resource_id: event.resource_id,
+      stage_id: event.stage_id,
+      status_id: event.status_id,
+    };
+    this.resourceForm.value.resources.push(ids);
+    this.addedResource.push(event);
+  }
+
+  deleteSelectedResource(i: number, sliding: IonItemSliding) {
+    this.resourceForm.value.resources.splice(i, 1);
+    this.addedResource.splice(i, 1);
+    sliding.close();
+  }
+
+  isPartnerFormValid() {
+    if (this.resourceForm.status == Status.INVALID) {
+      this.toastService.presentToast(this.toastConstants.all_required_field);
+      return;
+    }
+    if (this.addedResource.length == 0) {
+      this.toastService.presentToast(this.toastConstants.Invalid_Skill);
+      return;
+    }
+    return this.resourceForm.valid;
+  }
+}
---
+++ resource_management_mobile/src/app/shared/components/resource-card/resource-card.component.ts
@@ -0,0 +1,117 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { ToastConstants } from 'src/app/core/constant/toast.message.constant';
+import { ToastService } from 'src/app/core/toast/toast.service';
+import { ResourceRequirementService } from 'src/app/pages/resource-requirement/services/resource-requirement.service';
+import { CommonService } from '../../services/common.service';
+import { resourceId } from 'src/app/pages/resource-requirement/models/resource-requirement-model';
+import { resourceData, resourceResponse, stageData, stageResponse, statusData, statusResponse } from '../../models/common.model';
+
+
+@Component({
+  selector: 'app-resource-card',
+  templateUrl: './resource-card.component.html',
+  styleUrls: ['./resource-card.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, ReactiveFormsModule],
+})
+export class ResourceCardComponent implements OnInit {
+  @Input() resourceId: any;
+  resourceform!: FormGroup;
+  resourceData: any[] = [];
+  StatusData: statusData[] = [];
+  StageData: stageData[] = [];
+
+  resourceName!: string;
+  stageDescription!: string;
+  statusDescription!: string;
+
+  @Output() addResource = new EventEmitter();
+  constructor(
+    private modalController: ModalController,
+    private resourceRequirementService: ResourceRequirementService,
+    private toastConstants: ToastConstants,
+    private toastService: ToastService,
+    private commonService: CommonService
+  ) { }
+
+  ngOnInit() {
+    this.resourceform = new FormGroup({
+      resource_id: new FormControl('', Validators.required),
+      resource_name: new FormControl(''),
+      stage_id: new FormControl('', Validators.required),
+      stage_description: new FormControl(''),
+      status_id: new FormControl('', Validators.required),
+      status_description: new FormControl(''),
+    });
+    if (this.resourceId) {
+      this.getRequirementList();
+      this.getStatus();
+      this.getStage();
+    } else {
+      this.toastService.errorToast(this.toastConstants.invalid_requirement_ID)
+    }
+  }
+
+  setClose() {
+    this.modalController.dismiss();
+  }
+
+  onSubmit(form: FormGroup) {
+    if (form.valid) {
+      this.addResource.emit(form.value);
+      this.modalController.dismiss();
+    } else {
+      this.resourceform.markAllAsTouched();
+    }
+  }
+
+  getRequirementList() {
+    let requestRequirementId: resourceId = {
+      requirementId: this.resourceId
+    }
+    this.resourceRequirementService.getSigleRequirement(requestRequirementId).subscribe((resource: Object) => {
+      const resources = resource as resourceResponse;
+      if (resources.data.resourceInfo.length == 0) {
+        this.toastService.presentToast(this.toastConstants.resource_record_not_found)
+      } else {
+        this.resourceData = resources.data.resourceInfo
+      }
+    })
+  }
+
+  getStage() {
+    this.commonService.getStage().subscribe((stage: stageResponse) => {
+      this.StageData = stage.data.stageInfo;
+    })
+  }
+
+  getStatus() {
+    this.commonService.getStatus().subscribe((status: statusResponse) => {
+      this.StatusData = status.data.statusInfo;
+    })
+  }
+
+  selectedResourceValue(id: number) {
+    const resourceValue = this.resourceData.find((resource: resourceData) => id == resource.resource_id);
+    this.resourceform.patchValue({
+      resource_name: resourceValue.name
+    })
+  }
+
+  selectedStageValue(id: number) {
+    const stageValue = this.StageData.find((stage: stageData) => id == stage.stage_id);
+    this.resourceform.patchValue({
+      stage_description: stageValue?.description
+    })
+  }
+
+  selectedStatusValue(id: number) {
+    const statusValue = this.StatusData.find((status: statusData) => id == status.status_id);
+    this.resourceform.patchValue({
+      status_description: statusValue?.description
+    })
+  }
+}
