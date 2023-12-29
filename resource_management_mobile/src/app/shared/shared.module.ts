4370c052e8d11415b9501e0d37508edef43dc134
nallaiya <dineshbluesky1@gmail.com>
2023-12-29 14:59:41 +0530

    added menu highlight

--- resource_management_mobile/src/app/shared/shared.module.ts
+++ resource_management_mobile/src/app/shared/shared.module.ts
@@ -4,16 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { SharedPageRoutingModule } from './shared-routing.module';
 import { SharedPage } from './shared.page';
-import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { DateformatConverterPipe } from './helpers/pipes/dateformat-converter.pipe';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, SharedPageRoutingModule],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent],
   declarations: [
     SharedPage,
-    FooterComponent,
     HeaderComponent,
     DateformatConverterPipe,
   ],
